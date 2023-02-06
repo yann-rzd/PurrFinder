b3c190eece24492af0ba70a8d49db747748bd34a
Yann Rouzaud <yann.rouzaud@gmail.com>
2023-02-06 19:52:11 +0100

    Initial Commit

---
+++ PurrFinder/PurrFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurrFinderApp.swift
+//  PurrFinder
+//
+//  Created by Yann Rouzaud on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct PurrFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
