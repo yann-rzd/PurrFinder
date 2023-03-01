e46cb7ca424f8192f407816dd8947b790a1f5714
Yann Rouzaud <yann.rouzaud@gmail.com>
2023-03-01 16:59:20 +0100

    Adding tests for OpenAIAPICaller and LocationService

--- PurrFinderTests/LocationServiceTests.swift
+++ PurrFinderTests/LocationServiceTests.swift
@@ -1,14 +1,13 @@
 //
-//  PurrFinderTests.swift
+//  LocationServiceTests.swift
 //  PurrFinderTests
 //
-//  Created by Yann Rouzaud on 06/02/2023.
+//  Created by Yann Rouzaud on 01/03/2023.
 //
 
 import XCTest
-@testable import PurrFinder
 
-final class PurrFinderTests: XCTestCase {
+final class LocationServiceTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
