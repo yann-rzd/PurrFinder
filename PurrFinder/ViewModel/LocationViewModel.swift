8a612be8a8e07c489859d702ede8cc06ae0effe1
Yann Rouzaud <yann.rouzaud@gmail.com>
2023-03-06 20:07:45 +0100

    Change location update to every 10 minutes

--- PurrFinder/ViewModel/LocationViewModel.swift
+++ PurrFinder/ViewModel/LocationViewModel.swift
@@ -16,6 +16,7 @@ class LocationViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
     @Published var latitude: Double = 0
     @Published var longitude: Double = 0
     
+    private var locationUpdateTimer: Timer?
     
     // MARK: - INTERNAL: methods
     
@@ -24,6 +25,24 @@ class LocationViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
         locationManager?.delegate = self
         locationManager?.requestAlwaysAuthorization()
         locationManager?.startUpdatingLocation()
+
+        // Schedule location updates every 10 minutes
+        locationUpdateTimer = Timer.scheduledTimer(withTimeInterval: 600, repeats: true) { [weak self] _ in
+            self?.updateUserLocation()
+        }
+    }
+    
+    func updateUserLocation() {
+        let userUID = firebaseAuthService.getCurrentUserUID()
+        
+        Task {
+            do {
+                try await firestoreService.updateUserLocationData(userUID: userUID, latitude: latitude.description, longitude: longitude.description)
+            } catch {
+                self.error = error.localizedDescription
+                self.alert.toggle()
+            }
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -42,6 +61,22 @@ class LocationViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
                 self.alert.toggle()
             }
         }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            getUserLocation()
+        case .denied, .restricted, .notDetermined:
+            cancelLocationUpdateTimer()
+        @unknown default:
+            break
+        }
+    }
+    
+    private func cancelLocationUpdateTimer() {
+        locationUpdateTimer?.invalidate()
+        locationUpdateTimer = nil
     }
     
     
