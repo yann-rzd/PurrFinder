44a90e4f986e773e456a83dc051aa4c9f3aa39fa
Yann Rouzaud <yann.rouzaud@gmail.com>
2023-03-07 11:12:58 +0100

    Testing OpenAiService

---
+++ PurrFinderTests/OpenAIServiceTests.swift
@@ -0,0 +1,48 @@
+//
+//  PurrFinderTests.swift
+//  PurrFinderTests
+//
+//  Created by Yann Rouzaud on 06/02/2023.
+//
+
+import XCTest
+@testable import PurrFinder
+import Combine
+
+final class OpenAIServiceTests: XCTestCase {
+    
+    var openAIService: OpenAIService!
+    var cancellables = Set<AnyCancellable>()
+    
+    override func setUp() {
+        super.setUp()
+        openAIService = OpenAIService()
+    }
+    
+    override func tearDown() {
+        openAIService = nil
+        cancellables.removeAll()
+        super.tearDown()
+    }
+    
+    func testGivenAValidPrompt_WhenSendPrompt_ThenCompletionReceived() {
+        let expectation = XCTestExpectation(description: "Should return a response")
+        let message = "Hello, World!"
+        
+        openAIService.sendMessage(message: message)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    expectation.fulfill()
+                case .failure(let error):
+                    XCTFail("Error: \(error.localizedDescription)")
+                }
+            } receiveValue: { response in
+                // Vérifiez ici le contenu de la réponse
+                XCTAssertNotNil(response.choices)
+            }
+            .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 10.0)
+    }
+}
