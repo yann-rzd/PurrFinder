38d1eb212f627c62f037b3167e357d27556115c8
Yann Rouzaud <yann.rouzaud@gmail.com>
2023-03-11 16:10:50 +0100

    Storage bug test found

--- PurrFinderTests/StorageServiceTests.swift
+++ PurrFinderTests/StorageServiceTests.swift
@@ -13,23 +13,26 @@ import FirebaseAuth
 
 final class StorageServiceTests: XCTestCase {
     
+    override class func setUp() {
+        super.setUp()
+        Storage.storage().useEmulator(withHost: "localhost", port: 9199)
+    }
+    
     func testPersistProfileImageToStorage() async throws {
         let image = UIImage(systemName: "photo")!
-
+        
         let userUID = "7890"
-
-        let storage = Storage.storage()
-        storage.useEmulator(withHost: "localhost", port: 9199)
+        
         let storageService = StorageService()
         storageService.persistProfileImageToStorage(userUID: userUID, image: image)
         sleep(2)
-
-        let ref = storage.reference(withPath: "profileImages/\(userUID)")
+        
+        let ref = Storage.storage().reference(withPath: "profileImages/\(userUID)")
         let url = try await ref.downloadURL()
-
+        
         XCTAssertNotNil(url)
         XCTAssertNotEqual(url.absoluteString, "")
-
+        
         ref.delete { error in
             if let error = error {
                 print("Failed to delete file: \(error)")
@@ -37,28 +40,25 @@ final class StorageServiceTests: XCTestCase {
         }
     }
     
-//    func testPersistAnimalImageToStorage() async throws {
-//        let image = UIImage(systemName: "photo")!
-//
-//        let userUID = "4567"
-//
-//        let storage = Storage.storage()
-//        storage.useEmulator(withHost: "localhost", port: 9199)
-//        sleep(15)
-//        let storageService = StorageService()
-//        storageService.persistAnimalImageToStorage(userUID: userUID, image: image)
-//        sleep(2)
-//
-//        let ref = storage.reference(withPath: "animalImages/\(userUID)")
-//        let url = try await ref.downloadURL()
-//
-//        XCTAssertNotNil(url)
-//        XCTAssertNotEqual(url.absoluteString, "")
-//
-//        ref.delete { error in
-//            if let error = error {
-//                print("Failed to delete file: \(error)")
-//            }
-//        }
-//    }
+    func testPersistAnimalImageToStorage() async throws {
+        let image = UIImage(systemName: "photo")!
+        
+        let userUID = "7890"
+        
+        let storageService = StorageService()
+        storageService.persistAnimalImageToStorage(userUID: userUID, image: image)
+        sleep(2)
+        
+        let ref = Storage.storage().reference(withPath: "animalImages/\(userUID)")
+        let url = try await ref.downloadURL()
+        
+        XCTAssertNotNil(url)
+        XCTAssertNotEqual(url.absoluteString, "")
+        
+        ref.delete { error in
+            if let error = error {
+                print("Failed to delete file: \(error)")
+            }
+        }
+    }
 }
