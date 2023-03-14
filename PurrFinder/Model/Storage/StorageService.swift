976a5ffa1fb93ec03b51e19f368bb17ba36ba2e0
Yann Rouzaud <yann.rouzaud@gmail.com>
2023-03-14 14:51:07 +0100

    test for PR

--- PurrFinder/Model/Storage/StorageService.swift
+++ PurrFinder/Model/Storage/StorageService.swift
@@ -36,7 +36,6 @@ final class StorageService {
                 }
                 print("Successfully stored image with url : \(url?.absoluteString ?? "")")
             }
-            
         }
     }
     
